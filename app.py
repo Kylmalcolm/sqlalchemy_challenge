65046e4032dd49ec3d641c4d2f37803e237ceba9
Kylmalcolm <kylie.a.malcolm@gmail.com>
2020-12-01 22:19:50 -0600

    stations route updated

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 app = Flask(__name__)
 
@@ -6,7 +21,7 @@ app = Flask(__name__)
 def home():
     print("Server received request for home page...")
     return (
-        f"Welcome to the Hawaiian Weather API <br/>"
+        f"Hawaiian Weather API <br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
@@ -14,6 +29,20 @@ def home():
         f"/api/v1.0/YYYY-MM-DD<br/>"
         f"/api/v1.0/YYYY-MM-DD/YYYY-MM-DD"
     )
+
+# @app.route("/api/v1.0/precipitation")
+# def precipitation():
+#     session = Session(engine)
+#     prcp_12 = session.query(Measurement.prcp, Measurement.date)
+#     return jsonify
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    stations = session.query(Station.station).all()
+    session.close()
+    stations_list = list(np.ravel(stations))
+    return jsonify(stations_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
